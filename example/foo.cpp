286cc29beb3983023fb30acdb642698789cd7ea6
wjdrud2532 <wjdrud2532@naver.com>
2023-03-22 11:27:02 +0900

    test_03/22

---
+++ example/foo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  int x = 3;
+  int y = 4;
+  int z = x + y;
+  return z;
+}
---
+++ example/foo22.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+  int aaa = 5;
+
+  return 0;
+}
