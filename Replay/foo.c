ee97094c4dda4b82f48ac78283606fa5c493d367
yongilkim01 <yongil.k01@gmail.com>
2023-05-17 15:43:01 +0900

    replay

---
+++ Replay/foo.c
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	int xxxx = 10;
+	int yyyy = 5;
+	int zzzz = 20;
+
+	xxxx = 15; 
+	yyyy = 20;
+	zzzz = xxxx + yyyy;
+
+	return 0;
+}
---
+++ Replay/replay_header.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd.h> // for sleep function
+#include <ncurses.h>
+
+using namespace std;
