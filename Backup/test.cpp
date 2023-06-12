95f012ba5cd988c2783682690aa0e6d205ab79d7
wjdrud2532 <wjdrud2532@naver.com>
2023-06-12 19:09:54 +0900

    test record int, float, double, ll int type

---
+++ Backup/test.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int xxxxx = 5;
+  int yyyyy = 10;
+  int zzzzz = xxxxx + yyyyy;
+
+  int kkkkk;
+  float lllll = 999.999;
+  double ddddd = 888.888;
+  long long int ooooo = 7777777777777;
+
+  // char chaaaaaaar = 'A';
+  // string strrrrrr = "12345678";
+  // long int ooo;
+  // char ccccc;
+  // short sssss;
+
+  // cin >> sssss >> kkkkk >> lllll >> ddddd >> ooo >> ooooo;
+
+  cin >> kkkkk;
+  cin >> lllll;
+  cin >> ddddd;
+  cin >> ooooo;
+
+  cout << lllll << "\n";
+  // cout << ddddd << "\n";
+  // cout << ooooo << "\n";
+  // cout << chaaaaaaar << "\n";
+  // cout << strrrrrr << "\n";
+
+  // if (kkkkk < 10) {
+  //   cout << xxxxx << "aaa\n";
+  // } else {
+  //   cout << yyyyy << "bbb\n";
+  // }
+  return 0;
+}
