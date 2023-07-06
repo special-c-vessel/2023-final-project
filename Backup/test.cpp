e21bce3704235a6c0f2d6852c8ec1ba6eb5be248
wjdrud2532 <wjdrud2532@naver.com>
2023-07-06 22:04:55 +0900

    string 타입 일부 기록 테스트 코드 및 기록 내용

--- Backup/test.cpp
+++ Backup/test.cpp
@@ -1,73 +1,58 @@
-#include <cstdio>
-#include <fstream>
 #include <iostream>
-#include <sstream> // for split
 #include <string>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int tempfunc11111(int tempfucn_int, float tempfunc_float = 24.4444)
-{
-  tempfucn_int = 125;
-  tempfunc_float += tempfucn_int;
-
-  int tempfunc_arr[15] = {12, 13, 14, 15, 16, 17};
-  tempfunc_arr[14] = 9876543;
-
-  return tempfucn_int;
-}
-
-
 int main()
 {
-  int num_int = 5;
-  num_int++;
+//   string tempstr11111 = "";
+  
+//   string tempstr22222 = "hello";
 
-  float num_float = 123.15;
-  num_float += num_int;
+//   string tempstr33333 = "ewgaewgearg";
 
-  long long int num_llint = 12421522512;
-  num_llint = 9999999999999999;
+//   tempstr11111 = "aaaaaaa";
+//   tempstr11111 = "bbbbb";
 
-  char chr = 'G';
-  chr = 'Q';
+//   tempstr11111[2] = 'H';
+//   tempstr11111[3] = 'A';
 
-  if (num_int + num_float == 1111111)
-  {
-    num_llint = 11;
-  }
-  else
-  {
-    num_llint = 22;
-  }
+//   if(tempstr11111[3] == 'Q')
+// {
 
-  int arr_int[50] = { 33, 44, 55, 66, 77, 88 };
-  arr_int[30] = 99;
+// }
 
-  int arr_iint[30][30] = { {123, 456, 789}, {321, 654, 987}, };
-  arr_iint[15][16] = 44444;
-  arr_iint[15][16] = 99999;
+//   cout << tempstr11111 << "\n";
 
+  // FILE* fp;
+  // fp = fopen("HelloWorld.txt", "w+");
 
-  // int arr_iiiint[10][20][30] = {{{-1, -2, -3}, {-4, -5, -6}, {-77, -88, -99}}};
-  int arr_iiiint[10][20][30] = { {{-1, -2, -3}, {-4, -5, -6}} };
-  arr_iiiint[9][10][11] = 55555;
+  // int aaaaaaaa = 5;
 
-  char arr_charp[] = "hello, World!";
-  arr_charp[10] = 'B';
+  // char cccchhhhaaaarrrr[200] = "Hello World!";
+  
+  // int bbbbbbbbb = 10;
 
-  cout << arr_charp << "\n";
+  // fprintf(fp, "%s \n", cccchhhhaaaarrrr);
 
-  if (arr_iint[15][16] == 99999)
-  {
-    arr_iint[20][21] = 88888;
-  }
-  else
-  {
-    arr_iint[20][21] = 77777;
-  }
+  string s = "hello world";
 
-  int funcvalue = tempfunc11111(12);
+  // int aaaaa = 5;
+  // cout << s << "\n";
 
+  int num = 11111;
+  char tempstr[s.length() + 1];
+  int aaaa = 5;
+  s.copy(tempstr, s.length() + 1);
+  int bbbbb = 10;
+  // fprintf(fp, "%s \n", tempstr);
+  int ccccccc = 20;
+  s = "ggggggggggggg";
+  char tempstr2[s.length() + 1];
+  s.copy(tempstr2, s.length() + 1);
+  // fprintf(fp, "%s \n", tempstr2);
+  int dddddddddd = 33333;
+
+  return 0;
 }
