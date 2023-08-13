57127f3a2ae6df0fbc66626c11aeef397869a4fa
wjdrud2532 <wjdrud2532@naver.com>
2023-08-14 02:38:42 +0900

    segmentation fault가 발생하는 코드와 기록 예제

--- test.cpp
+++ test.cpp
@@ -1,46 +1,42 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-  int num1 = 5;
-  num1 = 55;
-  // cout << num1 << "\n";
+  int num1 = 10, num2 = 20, num3 = 30;
+  int * ptr1num1 = &num1;
+  int * tempptr2;
 
-  int * numptr = &num1;
-  int ** nnuummppttrr = &numptr;
-  int *** nnnptr = &nnuummppttrr;
+  int * ooooo[12];
+  int * arrptr[13][20];
+  int ** arrpptr[30][20];
+  
+  int temparr[20][40];
 
-  *numptr = 44;
-  **nnuummppttrr = 77;
-  ***nnnptr = 99;
+  arrptr[2][2] = &num1;
+  arrptr[10][10] = &num2;
 
-  // int num2 = 11111;
+  // segmentation fault !!!!!
+  // 그러나 기록됨
+  *ooooo[5] = 11;
 
-  // int arr[50] = {11, 22, 33, 44, 55};
+  arrpptr[15][13] = &ptr1num1;
 
-  // **nnuummppttrr = 77;
+  **arrpptr[15][13] = 99999;
 
-  // cout << num1 << "\n";
+  temparr[11][22] = 66666;
+  temparr[12][33] = 77777;
 
-  // temprecFunc("int", 2);
+  // segmentation fault !!!!!
+  // 이 아래로 기록되지 않음
+  *tempptr2 = 22222;
 
-  char char1 = 'a';
-  char1 = 'b';
-  char * charptr = &char1;
-  char ** cchhaarrptr = &charptr;
-
-  short short1 = 22;
-  short1 = 33;
-  short * shortptr = &short1;
-  short ** shortccc = &shortptr;
-
-  long long int lli1 = 11112123123213;
-  lli1 = 456453654363456;
-  long long int * lli1ptr = &lli1;
-  long long int ** lli222 = &lli1ptr;
-
+  temparr[12][35] = 88888;
+  temparr[12][36] = 99999;
+  
+  return 0;
 }
 
 
